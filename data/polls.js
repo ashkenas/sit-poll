45484e8e82374d7fe576f33dbb2e34279d1f0f8d
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-11-16 00:55:27 -0500

    Poll retrieval

--- data/polls.js
+++ data/polls.js
@@ -1,1 +1,62 @@
-module.exports = { };
+const { polls } = require("../config/mongoCollections");
+const { stringifyId, statusError } = require("../helpers");
+const { requireId } = require("../validation");
+
+const pollExists = async (id) => {
+    id = requireId(id, 'id');
+    const pollsCol = await polls();
+    const poll = await pollsCol.findOne({ _id: id }, { project: { _id: 1 } });
+    return poll !== null;
+};
+
+const requirePoll = (id) => (req, res, next) => {
+    req.params[id] = requireId(req.params[id], id);
+    pollExists(req.params[id]).then((exists) => {
+        if(!exists)
+            return next(statusError(404, 'Poll not found.'));
+        next();
+    }).catch(next);
+};
+
+const getPollById = async (id) => {
+    id = requireId(id, 'id');
+    const pollsCol = await polls();
+    const poll = await pollsCol.findOne({ _id: id });
+    if (!poll) return null;
+
+    poll.votes = poll.votes.map(stringifyId);
+    poll.reactions = poll.reactions.map(stringifyId);
+    poll.comments = poll.comments.map(stringifyId);
+    return stringifyId(poll);
+};
+
+const getPollSimplified = async (id) => {
+    id = requireId(id, 'id');
+    const pollsCol = await polls();
+    const poll = await pollsCol.findOne({ _id: id }, { project: { votes: 0, reactions: 0 } });
+    if (!poll) return null;
+
+    poll.votes = (await pollsCol.aggregate([
+        { $match: { _id: id } },
+        { $unwind: { path: '$votes' } },
+        { $group: { _id: '$votes.vote', count: { $count: {} } } },
+        { $sort: { _id: 1 } }
+    ]).toArray()).map((vote) => vote.count);
+
+    poll.reactions = (await pollsCol.aggregate([
+        { $match: { _id: id } },
+        { $unwind: { path: '$reactions' } },
+        { $group: { _id: '$reactions.reaction', count: { $count: {} } } }
+    ]).toArray()).map((reaction) => { return { reaction: reaction._id, count: reaction.count }; });
+
+    poll.comments = poll.comments.map(stringifyId);
+
+    return stringifyId(poll);
+};
+
+module.exports = {
+    getPollById,
+    getPollSimplified,
+    pollExists,
+    requirePoll
+};
--- routes/polls.js
+++ routes/polls.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { validate } = require('../validation');
 const router = express.Router();
 const path = require('path');
+const { getPollById, getPollSimplified } = require('../data/polls');
+const { statusError, sync } = require('../helpers');
+const { requirePoll } = require('../data').polls;
 
 const notImplemented = (res) => res.status(502).send({ error: 'Not implemented.' });
 
@@ -12,18 +15,20 @@ router
     });
 
 router
+    .use('/:id', requirePoll('id')) // Automatically 404s for all methods if necessary
     .route('/:id')
-    .get(validate(), async (req, res) => { // Get voting page for poll
-        // At this point, req.params.id is a valid ObjectId
-        res.render('polls/vote', { options: ['A', 'B', 'C'] })
-    })
-    .post(validate(['vote']), async (req, res) => { // Vote on poll
-        // At this point, req.params.id is a valid ObjectId
-        // At this point, req.body.vote is a valid number
+    .get(sync(async (req, res) => { // Get voting page for poll
+        res.render('polls/vote', { poll: await getPollById(req.params.id) });
+    }))
+    .post(validate(['vote']), sync(async (req, res) => { // Vote on poll
+        const poll = await getPollById(req.params.id);
+        const vote = req.body.vote;
+        if (vote < 0 || vote >= poll.choices.length)
+            throw statusError(400, 'Vote choice out-of-bounds.');
         // Vote logic into database would go here
         // Now that the vote has been processed, tell the webpage to redirect the user
         res.json({ redirect: path.join(req.originalUrl, 'results') });
-    })
+    }))
     .put(async (req, res) => { // Update poll
         notImplemented(res);
     })
@@ -38,10 +43,17 @@ router
     });
 
 router
+    .use('/:id/results', requirePoll('id')) // Automatically 404s for all methods if necessary
     .route('/:id/results')
-    .get(async (req, res) => { // Results page for poll
-        res.send('TODO: results page');
-    });
+    .get(sync(async (req, res) => { // Results page for poll
+        const poll = await getPollSimplified(req.params.id);
+        res.json(poll.choices.map((choice, i) => {
+            return {
+                choice: choice,
+                count: poll.votes[i]
+            };
+        }));
+    }));
 
 router
     .route('/:id/comment')
